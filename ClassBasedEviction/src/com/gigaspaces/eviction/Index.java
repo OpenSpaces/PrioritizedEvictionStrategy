5ba024892870320ec46270818da41f14b758d35d
sagioto <sagioto@gmail.com>
2012-08-08 14:00:19 +0300

    closes #6 the index and index value objects

---
+++ ClassBasedEviction/src/com/gigaspaces/eviction/Index.java
@@ -0,0 +1,40 @@
+package com.gigaspaces.eviction;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+public class Index{
+	private long major;
+	private AtomicLong minor;
+	
+	public Index() {
+		this.major = 0l;
+		this.minor = new AtomicLong(0);
+		
+	}
+
+	public IndexValue incrementAndGet(){
+		Long minorValue = getMinor().incrementAndGet();
+		
+		if(minorValue < 0)
+			synchronized (this) {
+				if(getMinor().get() < 0){
+					return new IndexValue(this.major++, 0l);
+				}
+			}
+		else return new IndexValue(getMajor(), minorValue);
+		//in the case the minor index was negative and upon entering the lock it 
+		//already is positive we get this thread to have another go at getting the index
+		return incrementAndGet();
+	}
+
+
+	public long getMajor() {
+		return major;
+	}
+
+
+	public AtomicLong getMinor() {
+		return minor;
+	}
+}
---
+++ ClassBasedEviction/src/com/gigaspaces/eviction/IndexValue.java
@@ -0,0 +1,34 @@
+package com.gigaspaces.eviction;
+
+public class IndexValue implements Comparable<IndexValue>{
+	private long major;
+	private long minor;
+
+	public IndexValue(Long major, Long minor) {
+		this.setMajor(major);
+		this.setMinor(minor);
+	}
+
+	public Long getMajor() {
+		return major;
+	}
+
+	public void setMajor(long major) {
+		this.major = major;
+	}
+
+	public Long getMinor() {
+		return minor;
+	}
+
+	public void setMinor(long minor) {
+		this.minor = minor;
+	}
+
+	@Override
+	public int compareTo(IndexValue o) {
+		long ans = this.getMajor() - o.getMajor();
+		return (int) ((ans == 0)? this.getMinor() - o.getMinor() : ans);
+	}
+
+}
