a1e933ad54f40bdcbb578646ef52602ca0a731a6
sagioto <sagioto@gmail.com>
2012-10-14 16:29:07 +0200

    added spaces

--- src/test/java/org/openspaces/eviction/test/data/Payload.java
+++ src/test/java/org/openspaces/eviction/test/data/Payload.java
@@ -18,15 +18,17 @@ public class Payload {
 	public Integer getId() {
 		return id;
 	}
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public byte[] getPayload() {
 		return payload;
 	}
+	
 	public void setPayload(byte[] payload) {
 		this.payload = payload;
 	}
-	
 	
 }
